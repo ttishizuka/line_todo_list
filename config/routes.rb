9acf027002a5a44fe825d46fbcf85e14fca152ff
TAIGA ISHIZUKA <>
2020-12-04 10:02:12 +0900

    scaffoldでtaskモデルの雛形を作成し、db:migrateでSQLを実行。 tasksコントローラーでindexを表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :tasks
+  root "tasks#index"
+
+  # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
